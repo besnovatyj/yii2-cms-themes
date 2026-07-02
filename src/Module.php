87e2c7e3ef32e1ecf1c674a57c098ff1c479a08c
besnovatyj <metr31@yandex.ru>
2026-07-02 23:14:51 +0500

    extract Kernel

--- src/Module.php
+++ src/Module.php
@@ -6,7 +6,7 @@
 
 namespace Besnovatyj\Themes;
 
-use common\components\module\CmsModule;
+use Besnovatyj\Kernel\module\CmsModule;
 use Besnovatyj\Contracts\module\DeclaresModule;
 use Besnovatyj\Contracts\module\ProvidesAdminMenu;
 
